0cc8339b4dca99f543eea8e5b4cf2c09368b7dc6
hadayan0 <fumano0@gmail.com>
2014-10-01 00:54:56 +0900

    add PHP-test file

---
+++ php.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>testtest</title>
+</head>
+<body>
+<h1>Welcome to my test webpage.</h1>
+<p>Hello, world.</p>
+<p>Here is my first page of github.io.</p>
+<?php
+print "<p>" . "This is a message by PHP." . "</p>" . PHP_EOL;
+?>
+</body>
+</html>
